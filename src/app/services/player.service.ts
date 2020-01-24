2f639f3f830d364d012f459b217badc08d23a333
Ivan Yakimov <yoowhi@gmail.com>
2020-01-24 03:55:42 +0300

    Audio playback implemented.

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -8,20 +8,40 @@ import {Track} from '../types/helpers';
 export class PlayerService {
   track: Track;
   playing: boolean;
+  audioContext: AudioContext;
+  tagID;
+  mediaElement: HTMLMediaElement;
+
 
   constructor(private queueService: QueueService) {
+    this.audioContext = new AudioContext();
   }
 
   playNext() {
     this.track = this.queueService.nextTrack();
+    this.startAudio();
   }
-// TODO
+
   playPrevious() {
     this.track = this.queueService.previousTrack();
+    this.startAudio();
   }
 
   togglePlay() {
+    this.playing ? this.mediaElement.pause() : this.mediaElement.play();
     this.playing = !this.playing;
-    // TODO
+  }
+
+  private startAudio() {
+    this.track.file.appendTo(this.tagID, {
+      controls: false
+    }, (err, elem) => {
+      const mediaSource = this.audioContext.createMediaElementSource(elem);
+      mediaSource.connect(this.audioContext.destination);
+      elem.onended = () => {
+        this.playNext(); // Is this a recursion?
+      };
+      this.mediaElement = elem;
+    });
   }
 }
